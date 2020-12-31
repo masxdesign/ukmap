9a2d4438d1ac08b4c21f7b9fea21c3a776367434
mauro <salgadom7530@gmail.com>
2020-12-31 03:45:11 +0000

    new

--- Gulpfile.js
+++ Gulpfile.js
@@ -11,11 +11,13 @@ const path = require('path')
 const through2 = require('through2')
 const File = require('vinyl')
 
-const { polygon, featureCollection } = require('@turf/helpers')
+const { polygon, featureCollection, feature } = require('@turf/helpers')
 const { default: union } = require('@turf/union')
+const { default: turfarea} = require('@turf/area')
 const { kebabCase } = require('lodash')
 const { default: polylabel } = require('polylabel')
 const lazypipe = require('lazypipe')
+
 
 
 function splitfiles(preprocessor)
@@ -75,56 +77,67 @@ function makeTransform2json(geojson)
     }
 }
 
-function convert(source, buildDir, json, geojson){
-    let res = src(`src/${source}`)
-        .pipe(replace(/^pc\(|\)$/i, ""))
-    
-    if(json) res = res.pipe(change(makeTransform2json(geojson)))
-        
-    return res.pipe(rename({ extname: ".json" }))
-        .pipe(dest(`build/${buildDir}`))
-}
+const flat2JsonTask = (geojson) => lazypipe()
+    .pipe(replace, /^pc\(|\)$/i, '')
+    .pipe(() => change(makeTransform2json(geojson)))()
 
 function clean()
 {
     return del(['build/**'])
 }
 
-function ukpoly2Flat()
-{
-    return convert("mp.js", "flat")
-}
-
-function ukpoly2Json()
-{
-    return convert("mp.js", "json", true)
-}
-
-function pcPoly2Flat()
-{
-    return convert("p/*.js", "flat/pc")
-}
-
-function pcPoly2Json()
-{
-    return convert("p/*.js", "json/pc", true)
-}
-
 function pcPoly2GeoJson()
 {
-    return convert("p/*.js", "single", true, true)
+    return src('src/p/*.js')
+        .pipe(flat2JsonTask(true))
+        .pipe(rename({ extname: ".json" }))
+        .pipe(dest(`build/postcodes`))
 }
 
 const regions = {
     "N Ireland": "BT",
     "Midlands": "CH,CW,HR,WR,DY,WV,TF,ST,WS,B,CV,DE,NG,LE,NN,PE,LN,NR",//"SY"
     "Wales": "NP,CF,SA,LL,LD,SY",
-    "Scotland": "AB,DD,EH,FK,G,IV,KA,KW,KY,ML,PA,PH,TD",
-    "North West England": "CA,LA,BB,PR,BL,OL,M,SK,WA,WN,L",
+    "Scotland": "AB,DD,EH,FK,G,IV,KA,KW,KY,ML,PA,PH,TD,DG",
+    "North West England": "CA,LA,BB,PR,BL,OL,M,SK,WA,WN,L,FY",
     "North East England": "NE,DH,SR,DL,TS,YO,HU,DN,S,HD,WF,HX,BD,LS,HG",
     "South East England": "OX,RG,SO,PO,GU,SL,HP,LU,MK,SG,AL,WD,HA,UB,TW,KT,RH,BN,TN,CT,ME,DA,BR,CR,SM,EN,CB,IP,CO,CM,SS,IG,RM",
     "South West England": "TR,PL,TA,DT,BH,SP,BA,BS,SN,GL,EX,TQ",
     "London": "EC,WC,W,SW,SE,E,N,NW"
+}
+
+function geojson__getCenter(feature)
+{
+    let center = []
+
+    if(feature.properties.label === 'South East England')
+    {
+        return [51.94540024,-0.01763881]
+    }
+
+    if(feature.geometry.type === "Polygon"){
+        center = polylabel(feature.geometry.coordinates)
+    }
+    else 
+    {
+        let maxArea = 0, maxPolygon = [];
+
+        for (let i = 0; i < feature.geometry.coordinates.length; i++)
+        {
+            const p = feature.geometry.coordinates[i];
+            const area = turfarea({type: "Polygon", coordinates: p})
+
+            if (area > maxArea)
+            {
+                maxPolygon = p;
+                maxArea = area;
+            }
+        }
+
+        center = polylabel(maxPolygon, '1.0')
+    }
+
+    return center.reverse()
 }
 
 function _generate_regions(data, addFile)
@@ -140,15 +153,18 @@ function _generate_regions(data, addFile)
         for (const postcode of postcodes) {
             if(!!pM[postcode])
             {
-                poly = polygon(pM[postcode], { label: postcode, polylabel: polylabel(pM[postcode]) })
-                combined = !combined ? poly: union(combined, poly)
+                let feature = polygon(pM[postcode], { label: postcode })
 
-                addFile({ basename: postcode, group: label, content: JSON.stringify(poly) })
+                feature.properties.center = geojson__getCenter(feature)
+                
+                combined = !combined ? feature: union(combined, feature)
+
+                addFile({ basename: postcode, group: label, content: JSON.stringify(feature) })
             }
         }
 
         combined.properties.label = label
-        combined.properties.polylabel = polylabel(combined.geometry.coordinates)
+        combined.properties.center = geojson__getCenter(combined)
 
         addFile({ basename, group: 'region', content: JSON.stringify(combined) })
     }
@@ -175,7 +191,7 @@ function generate_geojson()
             const filename = kebabCase(groupname)
             const lazyDest = lazypipe()
                 .pipe(jsoncombine, `${filename}.json`, combine_geojson)
-                .pipe(dest, 'build/postcodes/')
+                .pipe(dest, 'build/codes/')
     
             return gulpif((file) => file.group === groupname, lazyDest())
         })
@@ -185,7 +201,7 @@ function generate_geojson()
     pipeline = pipeline.pipe(() => {
         const lazyDest = lazypipe()
             .pipe(jsoncombine, `regions.json`, combine_geojson)
-            .pipe(dest, 'build/regions/')
+            .pipe(dest, 'build/')
 
         return gulpif((file) => file.group === 'region', lazyDest())
     })
@@ -195,10 +211,17 @@ function generate_geojson()
 
 function generate_regions(cb)
 {
-    src('build/json/mp.json')
+    src('src/mp.js')
+        .pipe(flat2JsonTask())
         .pipe(splitfiles(_generate_regions))
         .pipe(generate_geojson())
         .on('end', cb);
 }
 
-exports.default = series(clean, parallel(ukpoly2Flat, ukpoly2Json, pcPoly2Flat, pcPoly2Json, pcPoly2GeoJson), generate_regions)
+function deploy()
+{
+    return src('build/**')
+        .pipe(dest('../postcodemap/src/data/'))
+}
+
+exports.default = series(clean, parallel(pcPoly2GeoJson, generate_regions), deploy)
